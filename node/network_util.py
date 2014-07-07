143e153b90c054eeae482fc4499138b26f36f760
Julian Knutsen <mail@julianknutsen.com>
2014-07-06 22:41:37 -0700

    Add simple unit tests for node/network_util.py
    o Clean up node/network_util.py spacing and imports
    o Add run_tests.sh that will discover the python unit tests and run them. This
      could be expanded to run the behave tests in the future.
    o Add *.swp to .gitignore

--- node/network_util.py
+++ node/network_util.py
@@ -1,19 +1,15 @@
-import socket
-from struct import unpack
 import re
-
+import socket
+import struct
 
 def is_loopback_addr(addr):
     return addr.startswith("127.0.0.") or addr == 'localhost'
 
-
 def is_valid_port(port):
     return 0 < int(port) <= 65535
 
-
 def is_valid_protocol(protocol):
     return protocol == 'tcp'
-
 
 def is_valid_ip_address(addr):
     try:
@@ -22,14 +18,13 @@ def is_valid_ip_address(addr):
     except socket.error:
         return False
 
-
 def is_private_ip_address(addr):
     if is_loopback_addr(addr):
         return True
     if not is_valid_ip_address(addr):
         return False
     # http://stackoverflow.com/questions/691045/how-do-you-determine-if-an-ip-address-is-private-in-python
-    f = unpack('!I', socket.inet_pton(socket.AF_INET, addr))[0]
+    f = struct.unpack('!I', socket.inet_pton(socket.AF_INET, addr))[0]
     private = (
         [2130706432, 4278190080],  # 127.0.0.0,   255.0.0.0   http://tools.ietf.org/html/rfc3330
         [3232235520, 4294901760],  # 192.168.0.0, 255.255.0.0 http://tools.ietf.org/html/rfc1918
@@ -40,7 +35,6 @@ def is_private_ip_address(addr):
         if f & net[1] == net[0]:
             return True
     return False
-
 
 def uri_parts(uri):
     m = re.match(r"(\w+)://([\w\.]+):(\d+)", uri)
