549acff7446002a422102aa0fb465bdd51aa17f1
Brian Hoffman <hoffman_brian@bah.com>
2014-07-25 11:00:54 -0400

    Windows batch file

---
+++ run.bat
@@ -0,0 +1,11 @@
+﻿@echo off
+
+
+set "ip="
+for /f %%r in ('curl -s http://icanhazip.com') do (
+  set ip=%%r 
+)
+
+echo External IP: %ip%
+
+WinPython-64bit-2.7.6.4\python-2.7.6.amd64\python.exe Code\node\tornadoloop.py -p 12345 -s tcp://seed.openbazaar.org:12345 -l Code\logs\production.log -u 1 %ip%
