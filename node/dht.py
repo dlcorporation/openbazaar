a3a774366ea351dc63c424b1cd4c57e40010932b
Brian Hoffman <hoffman_brian@bah.com>
2014-08-15 19:50:29 -0400

    Check additional item

--- node/dht.py
+++ node/dht.py
@@ -112,7 +112,8 @@ class DHT(object):
             self._log.debug('New Peer')
             new_peer = self._transport.get_crypto_peer(guid, uri, pubkey, nickname)
             old_peer = self._routingTable.getContact(guid)
-            if old_peer._guid != new_peer._guid and old_peer._address != new_peer._address:
+
+            if old_peer is None or (old_peer._guid != new_peer._guid and old_peer._address != new_peer._address):
                 self._routingTable.addContact(new_peer)
             self._knownNodes.append((urlparse(uri).hostname, urlparse(uri).port, new_peer._guid))
             self._transport.save_peer_to_db(peer_tuple)
