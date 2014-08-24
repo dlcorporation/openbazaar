8cdf2422f95af1babd24f457eadee8e0ab42d0a4
Brian Hoffman <brianchoffman@gmail.com>
2014-08-23 23:02:03 -0400

    Check for empty list before retrieving

--- node/ws.py
+++ node/ws.py
@@ -486,7 +486,7 @@ class ProtocolHandler:
         self._log.info('Found Contracts: %s' % type(results))
         self._log.info(results)
 
-        if type(results[0]) == str:
+        if len(results) > 0 and type(results[0]) == str:
             results = json.loads(results[0])
 
         self._log.info(results)
