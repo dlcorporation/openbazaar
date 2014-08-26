d81ee42ca17c03c2a777f479f75a1168f9fb61fd
gubatron <gubatron@gmail.com>
2014-08-25 21:19:02 -0400

    new Backup.get_backups(folder) returns a list of (new) Backup objects.

--- node/backuptool.py
+++ node/backuptool.py
@@ -59,15 +59,72 @@ class BackupTool:
     @staticmethod
     def restore(backupTarFilepath):
         pass
-    
+
+
+class Backup:
+    """
+    A (meant to be immutable) POPO to represent a backup. So that we can tell
+    our Web client about the available backups made.
+    """
+    def __init__(self, file_name=None, 
+                 full_file_path=None, 
+                 created_timestamp_millis=None,
+                 size_in_bytes=None):
+        self.file_name = file_name
+        self.full_file_path = full_file_path
+        self.created_timestamp_millis = created_timestamp_millis
+        self.size_in_bytes = size_in_bytes
+
+    def __str__(self):
+        return "{ file_name: '%s', full_file_path: '%s', created_timestamp_millis: %d, size_in_bytes: %d}" % \
+            (self.file_name,
+             self.full_file_path,
+             long(self.created_timestamp_millis),
+             long(self.size_in_bytes))
+
+    @staticmethod
+    def get_backups(backups_folder_path):
+        '''
+        Returns a list of Backup objects found in the backup folder path given.
+        '''
+        result = []
+        if backups_folder_path is not None and os.path.isdir(backups_folder_path):
+            if backups_folder_path.endswith(os.sep):
+                backups_folder_path = backups_folder_path[:-1] #trim trailing '/'
+            result = [Backup.get_backup(backups_folder_path+os.sep+x) for x in os.listdir(backups_folder_path)]
+        return result
+
+    @staticmethod
+    def get_backup(backup_file_path):
+        result = None
+
+        try:
+            file_stat = os.stat(backup_file_path)
+            file_name = os.path.basename(backup_file_path)
+            created_timestamp = file_stat.st_ctime
+            size_in_bytes = file_stat.st_size
+
+            result = Backup(file_name,
+                            backup_file_path,
+                            created_timestamp,
+                            size_in_bytes)
+        except:
+            pass
+        return result
+
+
 if __name__ == '__main__':
+    # tests here.
     def onBackUpDone(backupFilePath):
-        print "Backup succeeded!\nOutput file at",backupFilePath
-        
+        print "Backup succeeded!\nOutput file at", backupFilePath
+
     def onError(errorMessage):
         print "Backup failed!", errorMessage
-    
+
     BackupTool.backup("/Users/gubatron/workspace.frostwire/OpenBazaar", 
                       "/Users/gubatron/workspace.frostwire/OpenBazaar/html/backups",
-                       onBackUpDone, 
-                       onError)
+                      onBackUpDone, 
+                      onError)
+    for x in Backup.get_backups("/Users/gubatron/workspace.frostwire/OpenBazaar/html/"):
+        print x
+
