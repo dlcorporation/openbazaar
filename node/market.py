6ff3daa07a7d513ecb3eaeefeb02503363fc2579
Dionysis Zindros <dionyziz@gmail.com>
2014-08-31 22:55:36 +0300

    Display correct proof-of-burn amounts for remote markets

--- node/market.py
+++ node/market.py
@@ -495,12 +495,16 @@ class Market(object):
         self._transport.send(msg, find_guid, callback)
 
     def on_page(self, page):
-        guid = page.get('guid')
+        guid = page.get('senderGUID')
 
         # TODO: allow async calling in different thread
         def reputation_pledge_retrieved(amount):
-            self._log.debug('Received reputation pledge amount %s for guid %s' % (guid, amount))
-            page['reputation_pledge'] = amount
+            self._log.debug('Received reputation pledge amount %s for guid %s' % (amount, guid))
+            SATOSHIS_IN_BITCOIN = 100000000
+            amountInSatoshis = amount
+            bitcoins = float(amount) / SATOSHIS_IN_BITCOIN
+            bitcoins = round(bitcoins, 4)
+            page['reputation_pledge'] = bitcoins
 
         trust.get_global(guid, reputation_pledge_retrieved)
         sin = page.get('sin')
