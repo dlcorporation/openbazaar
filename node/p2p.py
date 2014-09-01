dlcorporation/openbazaar

commit 4857c99c7bbf85f28c490f7f74f387c7d6e01a08

listen on ipv6 as well as ipv4
Signed-off-by: Thomas Stilwell <stilwellt@openlabs.co>

node/p2p.py
@@ -138,6 +138,7 @@ class TransportLayer(object):
                 raise Exception(error_message)
 
         else:
+            self.socket.ipv6 = True
             self.socket.bind('tcp://[*]:%s' % self._port)
 
         self.stream = zmqstream.ZMQStream(

commit 0acf59d87865fb1d88f1212edf0bf69fa0487b80

fail gracefully when listen socket can't do IPv6
fixes bug introduced in 4857c99c7bbf85f28c490f7f74f387c7d6e01a08
(listen on ipv6 as well as ipv4)

Signed-off-by: Thomas Stilwell <stilwellt@openlabs.co>

node/p2p.py
@@ -142,7 +142,10 @@ class TransportLayer(object):
                 raise Exception(error_message)
 
         else:
-            self.socket.ipv6 = True
+            try:
+                self.socket.ipv6 = True
+            except AttributeError:
+                pass
             self.socket.bind('tcp://[*]:%s' % self._port)
 
         self.stream = zmqstream.ZMQStream(