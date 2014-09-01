b8829fd23b34c2d7e892a88b08e62fd2da3979ca
Dionysis Zindros <dionyziz@gmail.com>
2014-09-01 21:23:02 +0300

    Proper return value for checkstyle script

--- checkstyle.sh
+++ checkstyle.sh
@@ -4,15 +4,17 @@ set -o pipefail
 
 # find . -iname "*.py"|xargs flake8 --ignore=E501,E127,F811,F821,F403 --exclude=*pybitmessage*,*pysqlcipher*
 
-ERR=0
+ERR=false
 
 for file in $(find . -iname "*.py" -o -iname "*.html" -o -iname "*.js"|grep -v pybitmessage|grep -v '.min.js'|grep -v bower_components); do
     if [ "$(tail -c1 $file)" != "" ]; then
         echo "$file: No new line at end of file"
-        ERR=1
+        ERR=true
     fi
 done
 
-if [ $ERR ]; then
+if $ERR ; then
     exit 1
+else
+    exit 0
 fi
