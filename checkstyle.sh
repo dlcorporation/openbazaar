4b68653ade1592b13f6c97087466cdd7eeac6119
Dionysis Zindros <dionyziz@gmail.com>
2014-09-03 22:57:33 +0300

    Add jshinting

--- checkstyle.sh
+++ checkstyle.sh
@@ -2,9 +2,21 @@
 set -o errexit
 set -o pipefail
 
-find . -iname "*.py" -not -path "./env/*"|xargs flake8 --ignore=E501,E127,F811,F821,F403 --exclude=*pybitmessage*,*pysqlcipher*
-
 ERR=false
+
+for file in $(find . -iname "*.py" -not -path "./env/*"|grep -v pybitmessage|grep -v pysqlcipher); do
+    echo "Checking python source: $file"
+    if ! flake8 --ignore=E501,E127,F811,F821,F403 $file; then
+        ERR=true
+    fi
+done
+
+for file in $(find . -not -path "./env/*" -and '(' -iname "*.js" ')'|grep -v pybitmessage|grep -v '.min.js'|grep -v bower_components); do
+    echo "Checking JS source: $file"
+    if ! jshint $file; then
+        ERR=true
+    fi
+done
 
 for file in $(find . -not -path "./env/*" -and '(' -iname "*.py" -o -iname "*.html" -o -iname "*.js" ')'|grep -v pybitmessage|grep -v '.min.js'|grep -v bower_components); do
     if [ "$(tail -c1 $file)" != "" ]; then
