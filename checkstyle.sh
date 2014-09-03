a29793a5fec379d12ac9483ff7c82fa7abf1cd99
Dionysis Zindros <dionyziz@gmail.com>
2014-09-04 02:08:36 +0300

    Refactor checkstyle.sh

--- checkstyle.sh
+++ checkstyle.sh
@@ -4,26 +4,45 @@ set -o pipefail
 
 ERR=false
 
-for file in $(find . -iname "*.py" -not -path "./env/*"|grep -v pybitmessage|grep -v pysqlcipher); do
-    echo "Checking python source: $file"
-    if ! flake8 --ignore=E501,E127,F811,F821,F403 $file; then
-        ERR=true
-    fi
-done
+function python_check() {
+    for file in $(find . -iname "*.py" -not -path "./env/*"|grep -v pybitmessage|grep -v pysqlcipher); do
+        echo "Checking python source: $file"
+        if ! flake8 --ignore=E501,E127,F811,F821,F403 $file; then
+            ERR=true
+        fi
+    done
+}
 
-for file in $(find . -not -path "./env/*" -and '(' -iname "*.js" ')'|grep -v pybitmessage|grep -v '.min.js'|grep -v bower_components); do
-    echo "Checking JS source: $file"
-    if ! jshint $file; then
-        ERR=true
-    fi
-done
+function js_check() {
+    for file in $(find . -not -path "./env/*" -and '(' -iname "*.js" ')'|grep -v pybitmessage|grep -v '.min.js'|grep -v bower_components); do
+        echo "Checking JS source: $file"
+        if ! jshint $file; then
+            ERR=true
+        fi
+    done
+}
 
-for file in $(find . -not -path "./env/*" -and '(' -iname "*.py" -o -iname "*.html" -o -iname "*.js" ')'|grep -v pybitmessage|grep -v '.min.js'|grep -v bower_components); do
-    if [ "$(tail -c1 $file)" != "" ]; then
-        echo "$file: No new line at end of file"
-        ERR=true
-    fi
-done
+
+function new_line_check() {
+    for file in $(find . -not -path "./env/*" -and '(' -iname "*.py" -o -iname "*.html" -o -iname "*.js" ')'|grep -v pybitmessage|grep -v '.min.js'|grep -v bower_components); do
+        if [ "$(tail -c1 $file)" != "" ]; then
+            echo "$file: No new line at end of file"
+            ERR=true
+        fi
+    done
+}
+
+case $1 in
+python)
+    python_check
+    ;;
+js)
+    js_check
+    ;;
+*)
+    python_check
+    js_check
+esac
 
 if $ERR ; then
     exit 1
