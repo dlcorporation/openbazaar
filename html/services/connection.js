bee88732fa647f8456fcf75e397c01e57bb8fe91
Dionysis Zindros <dionyziz@gmail.com>
2014-09-04 02:35:26 +0300

    JS style fixes on connection service

--- html/services/connection.js
+++ html/services/connection.js
@@ -5,33 +5,33 @@ angular.module('app')
       var websocket = new WebSocket(socket_uri);
 
       websocket.onopen = function(evt) {
-        self.websocket.send(JSON.stringify({"id":42, "command":"load_page", "params":{}}))
-        self.websocket.send(JSON.stringify({"id":42, "command":"check_order_count", "params":{}}))
-        //self.websocket.send(JSON.stringify({"id":42, "command":"read_log", "params":{}}))
-      }
+        self.websocket.send(JSON.stringify({"id":42, "command":"load_page", "params":{}}));
+        self.websocket.send(JSON.stringify({"id":42, "command":"check_order_count", "params":{}}));
+        //self.websocket.send(JSON.stringify({"id":42, "command":"read_log", "params":{}}));
+      };
 
       websocket.onclose = function(evt) {
-       console.log("closed", evt)
+       console.log("closed", evt);
        console.log('The websocket closed unexpectedly. Refreshing.');
-       window.location.reload()
-      }
+       window.location.reload();
+      };
 
       websocket.onerror = function(evt) {
-       console.log("error", evt)
-      }
+       console.log("error", evt);
+      };
 
       websocket.onmessage = function(evt) {
-        var data = JSON.parse(evt.data)
-        //console.log("Websocket.onMessage!")
-        //console.log(data)
-        onMessage(data.result)
-      }
+        var data = JSON.parse(evt.data);
+        //console.log("Websocket.onMessage!");
+        //console.log(data);
+        onMessage(data.result);
+      };
       this.websocket = websocket;
       var self = this;
 
       this.send = function(command, msg) {
        if (msg === undefined) {
-         msg = {}
+         msg = {};
        }
        
          var request = {
@@ -44,8 +44,8 @@ angular.module('app')
         //console.log('Connection.send ->')
         //console.log(message)
         self.websocket.send(message);
-      }
-    }
+      };
+    };
 
     var scope = $rootScope.$new(true);
 
