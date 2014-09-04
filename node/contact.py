4b15fd36c75004cb90d6bd9ec0cf6dc2fe8e7a1f
gubatron <gubatron@gmail.com>
2014-09-04 17:39:53 -0400

    license headers unnecessary

--- node/contact.py
+++ node/contact.py
@@ -1,25 +1,3 @@
-"""
-    This file is part of OpenBazaar.
-
-    OpenBazaar is an open source project to create a decentralized network for
-    commerce online that has no fees and cannot be censored.
-
-    Copyright (C) 2014  The OpenBazaar Team
-
-    OpenBazaar is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as
-    published by the Free Software Foundation, either version 3 of the
-    License, or (at your option) any later version.
-
-    OpenBazaar is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
-
 class Contact(object):
     """ Encapsulation for remote contact
 
