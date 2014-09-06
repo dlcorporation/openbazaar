dlcorporation/openbazaar

commit f1a1f1e52f46842f4991c4d74a057fc104863d5f

WIP DHT fixup

node/crypto2crypto.py
@@ -613,6 +613,12 @@ class CryptoTransportLayer(TransportLayer):
         if send_to is not None:
 
             peer = self.dht._routingTable.getContact(send_to)
+            if not peer:
+                for activePeer in self.dht.activePeers:
+                    if activePeer._guid == send_to:
+                        peer = activePeer
+                        break
+
             # peer = CryptoPeerConnection(msg['uri'])
             if peer:
                 self.log.debug('Directed Data (%s): %s' % (send_to, data))

node/routingtable.py
@@ -539,11 +539,11 @@ class OptimizedTreeRoutingTable(TreeRoutingTable):
             self.buckets[bucketIndex].removeContact(contactID)
             # Replace this stale contact with one from our replacement
             # cache, if we have any
-            # if self.replacementCache.has_key(bucketIndex):
-            #     if len(self.replacementCache[bucketIndex]) > 0:
-            #         self.buckets[bucketIndex].addContact(
-            #             self.replacementCache[bucketIndex].pop()
-            #         )
+            if bucketIndex in self._replacementCache:
+                if len(self.replacementCache[bucketIndex]) > 0:
+                    self.buckets[bucketIndex].addContact(
+                        self.replacementCache[bucketIndex].pop()
+                    )
 
             self.log.debug(
                 'Contacts: %s' % self.buckets[bucketIndex].contacts

commit a078d3b8614729d94ec39f9f608517cbbbcf2d4e

find/replace a few more changed variable names
fixes bug caused by f1a1f1e52f46842f4991c4d74a057fc104863d5f (WIP DHT
fixup)

emphasis on the WIP ;)

Signed-off-by: Thomas Stilwell <stilwellt@openlabs.co>

node/crypto2crypto.py
@@ -615,7 +615,7 @@ class CryptoTransportLayer(TransportLayer):
             peer = self.dht._routingTable.getContact(send_to)
             if not peer:
                 for activePeer in self.dht.activePeers:
-                    if activePeer._guid == send_to:
+                    if activePeer.guid == send_to:
                         peer = activePeer
                         break
 

node/routingtable.py
@@ -539,7 +539,7 @@ class OptimizedTreeRoutingTable(TreeRoutingTable):
             self.buckets[bucketIndex].removeContact(contactID)
             # Replace this stale contact with one from our replacement
             # cache, if we have any
-            if bucketIndex in self._replacementCache:
+            if bucketIndex in self.replacementCache:
                 if len(self.replacementCache[bucketIndex]) > 0:
                     self.buckets[bucketIndex].addContact(
                         self.replacementCache[bucketIndex].pop()