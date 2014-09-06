e467a551e53d035b319de0c385bfca4de5008acd
Bryan Stitt <bryan@stitthappens.com>
2014-09-05 19:17:53 -0700

    remove extraneous semicolon

--- configure.sh
+++ configure.sh
@@ -21,7 +21,7 @@ set -e
 function command_exists {
   #this should be a very portable way of checking if something is on the path
   #usage: "if command_exists foo; then echo it exists; fi"
-  type "$1" &> /dev/null ;
+  type "$1" &> /dev/null
 }
 
 function installMac {
