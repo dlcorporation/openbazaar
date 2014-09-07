697a94bc2a4aaa1159a6023669bff0b3cab13f66
gubatron <gubatron@gmail.com>
2014-09-07 12:41:48 -0400

    Backuptool update.
     - No need to backup the msig folder, as keys are saved in the
       gpg keyring. Just backing up the db/ folder.
     - I, Angel Leon, I'm the original author of this file and I
       remove the GPL header block so that other open source license
       can be applied to this file, preferably the MIT license,
       as described on the LICENSE.txt file of the OpenBazaar project.
     - pep8 fixes.

--- node/backuptool.py
+++ node/backuptool.py
@@ -1,11 +1,4 @@
 #!/usr/bin/env python
-#
-# This library is free software, distributed under the terms of
-# the GNU Lesser General Public License Version 3, or any later version.
-# See the COPYING file included in this archive
-#
-# The docstrings in this module contain epytext markup; API documentation
-# may be created by processing this file with epydoc: http://epydoc.sf.net
 import tarfile
 import time
 import os
@@ -20,21 +13,28 @@ class BackupTool:
     Created August 23, 2014.
     """
     @staticmethod
-    def backup(openbazaarInstallationPath, backupFolderPath, onSucessCallback=None, onErrorCallback=None):
+    def backup(openbazaarInstallationPath,
+               backupFolderPath,
+               onSucessCallback=None,
+               onErrorCallback=None):
         """
         Creates an 'openbazaar-YYYY-MM-DD-hh-mm-ss.tar.gz' file
         inside the html/backups/ folder.
         folder.
 
-        openbazaarInstallationPath : String -> The path to OpenBazaar's installation, where the db/ and msig/ folders live.
-        backupFolderPath : String -> The folder where the backup file will exist.
+        openbazaarInstallationPath : String -> The path to OpenBazaar's
+        installation folder, where the db/ folder lives.
+
+        backupFolderPath : String -> The folder where the backup
+        file will exist.
 
         Optional callback functions can be passed.
             onSucessCallback(backupFilePath : String)
             onErrorCallback(errorMessage : String)
         """
         dateTime = time.strftime('%Y-%h-%d-%H-%M-%S')
-        outputFilepath = backupFolderPath + os.sep + "openbazaar-%s.tar.gz" % dateTime
+        outputFilepath = backupFolderPath + os.sep +\
+            "openbazaar-%s.tar.gz" % dateTime
 
         try:
             os.makedirs(backupFolderPath)
@@ -45,9 +45,7 @@ class BackupTool:
         try:
             tar = tarfile.open(outputFilepath, "w:gz")
             db_folder = openbazaarInstallationPath + os.sep + "db"
-            msig_folder = openbazaarInstallationPath + os.sep + "msig"
             tar.add(db_folder, os.path.basename(db_folder))
-            tar.add(msig_folder, os.path.basename(msig_folder))
             tar.close()
         except Exception as e:
             if onErrorCallback is not None:
@@ -66,18 +64,22 @@ class BackupTool:
     @staticmethod
     def get_installation_path():
         """Returns the Project Root path"""
-        return os.path.realpath(os.path.abspath(__file__))[:os.path.realpath(os.path.abspath(__file__)).find('/node')]
+        file_abs_path = os.path.abspath(__file__)
+        real_file_abs_path = os.path.realpath(file_abs_path)
+        return real_file_abs_path[:real_file_abs_path.find('/node')]
 
     @staticmethod
     def get_backup_path():
-        """TODO: Make backup path configurable on server settings before run.sh"""
-        return BackupTool.get_installation_path() + os.sep + 'html' + os.sep + 'backups'
+        """TODO: Make backup path configurable on server settings"""
+        return BackupTool.get_installation_path() +\
+            os.sep + 'html' + os.sep + 'backups'
 
 
 class Backup(json.JSONEncoder):
     """
-    A (meant to be immutable) POPO to represent a backup. So that we can tell
-    our Web client about the available backups made.
+    A (meant to be immutable) POPO to represent a backup.
+    So that we can tell our Web client about the
+    available backups available.
     """
     def __init__(self, file_name=None,
                  full_file_path=None,
@@ -107,11 +109,14 @@ class Backup(json.JSONEncoder):
         Returns a list of Backup objects found in the backup folder path given.
         '''
         result = []
-        if backups_folder_path is not None and os.path.isdir(backups_folder_path):
+        if backups_folder_path is not None\
+           and os.path.isdir(backups_folder_path):
             if backups_folder_path.endswith(os.sep):
                 # trim trailing '/'
                 backups_folder_path = backups_folder_path[:-1]
-            result = reversed([Backup.get_backup(backups_folder_path + os.sep + x) for x in os.listdir(backups_folder_path)])
+            result = reversed([Backup.get_backup(backups_folder_path +
+                                                 os.sep + x)
+                               for x in os.listdir(backups_folder_path)])
 
         return result
 
