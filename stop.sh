667e72a6b33f693f4638b2698a08c14f2dd3ec67
Rav3nPL <rav3n.pl@gmail.com>
2014-09-14 12:53:23 +0200

    More intellignet shutdown
    Check every second by 5 seconds then kill if need.

--- stop.sh
+++ stop.sh
@@ -2,7 +2,16 @@
 for pid in `ps aux | grep "python.*openbazaar_daemon.py" | grep -v grep | awk '{print $2}'`; do
   echo "Sending SIGTERM to ${pid}"
   kill ${pid}
-  sleep 5s
+  for w in {1..5} 
+  do
+    if ps -p $pid > /dev/null
+    then
+      echo "Waiting ${w}s..."
+      sleep 1s
+    else
+      break
+    fi
+  done  
   if ps -p $pid > /dev/null
     then
     echo "Still running, sending SIGKILL to ${pid}"
