013805f7a16a08d992f2587e1dc10fe08e1732d7
Ignat Ignatov <ignat99@gmail.com>
2014-09-16 02:38:47 +0200

    [FEATURE] openbazaar port for OSX and GNU/Linux Ubuntu/Debian

--- port.sh
+++ port.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-PORT=`lsof -i -n -P -i4 | grep TCP | grep python | grep \* | grep -v 12345 | awk ' {print $9}' | awk 'BEGIN{FS = "[:]"} {print $2}'`
+PORT=`lsof -i -n -P -i4 | grep TCP | grep python | grep -v \> | grep -v 12345 | awk ' {print $9}' | awk 'BEGIN{FS = "[:]"} {print $2}'`
 echo "http://localhost:$PORT"
