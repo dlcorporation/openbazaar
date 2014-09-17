b569099732798caa4813747e83d869686814b1d4
Nikolaos Korasidis <renelvon@gmail.com>
2014-09-17 15:32:15 +0300

    Simplify protocol specification.

--- node/util.py
+++ node/util.py
@@ -1,23 +1,23 @@
 import webbrowser
 
 
-def open_default_webbrowser(url, protocol_prefix="http://"):
+def open_default_webbrowser(url, protocol="http"):
     """
     Open URL at the default browser.
 
     @param url: The URL to open.
     @type url: str
 
-    @param protocol_prefix: The internet protocol to use.
-    @type protocol_prefix: str
+    @param protocol: The internet protocol to use.
+    @type protocol: str
 
     @return: True on success, False on failure.
     @rtype: bool
     """
-    if not url.startswith(protocol_prefix):
+    if not url.startswith(protocol):
         # If protocol is absent from the url, attach it, otherwise
         # the file `url` will be opened in Linux flavors.
-        full_url = protocol_prefix + url
+        full_url = "%s://%s" % (protocol, url)
     else:
         full_url = url
 
